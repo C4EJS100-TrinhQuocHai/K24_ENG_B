e6549c5e7748dab97ed306a6e21f5dce8e4605cb
trinhquochai <trinhquochai12@gmail.com>
2024-12-13 15:29:08 +0700

    session16_pointer

---
+++ session15/pointerwitdharrayc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+	con tro voi mang
+	dia chi cua mang se trung voi gia tri cua phan tu thu nhat.
+*/
+int main() {
+   int arr[]={2,4,6,8};// tao arr co 4 phan tu 
+//   printf("gia tri dia chi cua arr la: %d \n",&arr);
+printf("gia tri dia chi cua phan tu 3: %d ",&arr[3]);
+   int *ptr;
+   ptr= arr;
+//   printf("gia tri dia chi cua bien con tro ptr la %d \n",ptr);
+   
+   
+   
+
+  return 0;
+}
+
---
+++ session15/vd1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void changeCount(int *x);
+// khai bao ham nguyen mau
+int main() {
+   int count=5;
+//   printf("dia chi cua bien count la : %d \n",&count);//6487580
+   printf("gia tri cua bien count truoc: %d \n",count);
+   changeCount(&count); //PASS BY REFERENCE
+   printf("gia tri cua bien count sau: %d \n" ,count);
+
+  return 0;
+}
+// noi di trien khai cac ham
+  changeCount(int *x){
+  	 *x=15;
+//  	printf(" dia chi cua x: %d \n",x);
+  }
+
