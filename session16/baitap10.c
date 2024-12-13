891441c4d177fe41437da900e0d0b65604500339
trinhquochai <trinhquochai12@gmail.com>
2024-12-13 15:33:09 +0700

    session16_pointer

---
+++ session16/baitap10.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/*
+	khai bao ham kiem tra xem ki tu duoc in ra voi so lan co ton tai trong 
+	mang minh da khai bao chua
+*/
+ int check(char n, char arr[]){
+ 	 int length= strlen(arr);
+ 	 for(int i=0; i<length; i++){
+ 	 	if(arr[i]==n){
+ 	 		return 0;
+ 	 	}
+ 	 }
+ 	return 1;// chung to no khong ton tai trong mang checked
+ }
+int main() {
+  char str[]="bbbcdac";
+  char checked[50];
+  int index=0;
+  int length= strlen(str);
+   for(int i=0; i<length; i++){
+   	 int count=0;
+   	 if(check(str[i],checked)){
+   	 	for(int j=i; j<length; j++){
+   	 		if(str[i]==str[j]){
+   	 			count++;
+   	 		}
+   	 	}
+   	 	// in ra
+   	 	printf(" %c xuat hien so lan la: %d \n",str[i],count);
+   	 	checked[index]=str[i];
+   	 	index++;
+   	 }
+   	
+   }
+  
+  
+  return 0;
+}
+
---
+++ session16/pointer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+	con tro (pointer) 
+	
+*/
+int main() {
+    int a=5;
+    int *ptr;// pointer : tao bien con tro co ten ptr
+    printf("dia chi cua bien a trong bo nho ram cua may tinh %d: \n",&a);
+    ptr=&a;// gan dia chi cua bien a cho con tro ptr;
+    printf("dia chi cua con tro ptr la %d \n",ptr);
+    *ptr=15;
+    // gia tri cua bien a se 15;
+    printf("gia tri cua bien a sau: %d",a);
+
+  return 0;
+}
+
